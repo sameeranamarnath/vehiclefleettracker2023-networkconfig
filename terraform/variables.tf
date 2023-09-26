16be3b9851b35546c035407d710f390b0d4b917c
sameeranamarnath <sameeranamarnath@gmail.com>
2023-09-26 15:09:45 +0530

    terraform vars and  main added

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region"{}
+variable "app_name" {
+    default="vehiclefleettracker2023-networkconfig"
+}
+
+variable "public_subnets"{
+    default = ["10.10.100.0/24","10.10.101.0/24"]
+    }
+    
+
+variable "private_subnets"{
+    default= ["10.10.0.0./24","10.10.1.0/24"]
+    }
+
+
+variable "availability_zones"{
+    default=["us-east-1a","us-east-1b","us-east-1c"]
+}
+
+
+
