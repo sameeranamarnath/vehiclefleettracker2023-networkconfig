dc1877151ebbc74d3fd71fb82e3d5c690c7ddebd
sameeranamarnath <sameeranamarnath@gmail.com>
2023-09-28 01:31:09 +0530

    hashicorp error fix aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,13 @@ terraform {
     required_providers {
       aws = {
         source ="hashicorp/aws"
-        version =" ~> 2.0" 
+        version =" ~> 4.0" 
       }
     }
 
   backend "s3" {}
 }
+
 
 provider "aws"{
  
