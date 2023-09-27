d747139fedd2c99040a97acd91f44415b2d02998
sameeranamarnath <sameeranamarnath@gmail.com>
2023-09-28 01:11:36 +0530

    hashicorp error fix aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,14 @@ terraform {
   backend "s3" {}
 }
 
-
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
+  }
+}
 provider "aws"{
  
  region = "${var.aws_region}"
