a8b277e9fc81a7e56cb94615a2cee78bd4b24e48
amar.s <sameeranamarnath@gmail.com>
2023-11-22 14:04:20 +0530

    config changes

--- terraform/eip.tf
+++ terraform/eip.tf
@@ -1,13 +1,15 @@
 resource "aws_eip" "eip_1" {
 
-    depends_on = [ aws_internet_gateway.igw ]
+  depends_on = [aws_internet_gateway.igw]
 
 
 }
 
+#changes
+
 resource "aws_eip" "eip_2" {
 
-    depends_on = [ aws_internet_gateway.igw ]
+  depends_on = [aws_internet_gateway.igw]
 
-    
+
 }
