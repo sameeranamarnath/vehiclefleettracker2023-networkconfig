eb94131b61f9d0e2cd2a845322f15fb371e81668
amar.s <sameeranamarnath@gmail.com>
2023-11-22 17:04:48 +0530

    changed app name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,22 @@
-variable "aws_region"{
-    default="us-east-1" #bugfix
+variable "aws_region" {
+  default = "us-east-1" #bugfix
 }
 variable "app_name" {
-    default="vehiclefleettracker2023-networkconfig"
+  default = "vehiclefleettracker2023"
 }
 
-variable "public_subnets"{
-    default = ["10.10.100.0/24","10.10.101.0/24"]
-    }
-    
-
-variable "private_subnets"{
-    default= ["10.10.0.0/24","10.10.1.0/24"]
-    }
+variable "public_subnets" {
+  default = ["10.10.100.0/24", "10.10.101.0/24"]
+}
 
 
-variable "availability_zones"{
-    default=["us-east-1a","us-east-1b","us-east-1c"]
+variable "private_subnets" {
+  default = ["10.10.0.0/24", "10.10.1.0/24"]
+}
+
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 
